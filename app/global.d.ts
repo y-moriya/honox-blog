f05203048ca18329ffa535ee25e47cc05beba73e
Yu MORIYA <euro21st@gmail.com>
2025-01-01 14:00:50 +0900

    .gitignoreにLocal Netlifyフォルダーを追加; global.d.tsのインポート文を修正し、Propsインターフェースを追加

--- app/global.d.ts
+++ app/global.d.ts
@@ -1,17 +1,27 @@
-import { } from 'hono'
-import type { Frontmatter } from './types/frontmatter'
+import { } from "hono";
+import type { Frontmatter } from "./types/frontmatter";
 
 type Head = {
   title?: string;
   frontmatter?: Frontmatter;
-}
+};
 
-declare module 'hono' {
+declare module "hono" {
   interface Env {
-    Variables: {}
-    Bindings: {}
+    // biome-ignore lint/complexity/noBannedTypes: <explanation>
+    Variables: {};
+    // biome-ignore lint/complexity/noBannedTypes: <explanation>
+    Bindings: {};
+  }
+  interface Props {
+    title?: string;
+    frontmatter?: Frontmatter;
   }
   interface ContextRenderer {
-    (content: string | Promise<string>, head?: Head): Response | Promise<Response>
+    // biome-ignore lint/style/useShorthandFunctionType: <explanation>
+    (
+      content: string | Promise<string>,
+      head?: Head,
+    ): Response | Promise<Response>;
   }
 }
