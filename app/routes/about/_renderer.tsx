ea1f0aa4493ec7aa3882a485eb8fe54edb13e16c
Yu MORIYA <euro21st@gmail.com>
2025-01-11 18:59:56 +0900

    自己紹介セクションを追加し、日付をフォーマットして表示。投稿の説明を更新。

--- app/routes/about/_renderer.tsx
+++ app/routes/about/_renderer.tsx
@@ -1,11 +1,19 @@
+import { format } from "date-fns";
 import { jsxRenderer } from "hono/jsx-renderer";
 
 export default jsxRenderer(({ children, Layout, frontmatter }) => {
+	const formattedDate = frontmatter?.date
+		? format(new Date(frontmatter.date), "yyyy/MM/dd")
+		: "";
 	return (
 		<Layout title={frontmatter?.title}>
 			<div class="container mx-auto flex-1 px-0 main-container">
 				<div class="max-w-8xl mx-auto px-0 sm:px-6 lg:px-8 markdown">
 					<h1 class="text-3xl font-bold">{frontmatter?.title}</h1>
+				</div>
+				<div class="max-w-8xl mx-auto p-0 sm:px-6 lg:px-8 markdown">
+					<p>{frontmatter?.description}</p>
+					<p class="text-center text-sm text-gray-500">{formattedDate}</p>
 				</div>
 				<div class="max-w-8xl mx-auto px-0 py-4 sm:px-6 lg:px-8 markdown">
 					{children}
